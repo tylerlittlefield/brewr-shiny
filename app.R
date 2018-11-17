b9aeda569ab03a5d840db5a699d511706e437506
tylurp <tylurp1@gmail.com>
2018-11-16 16:34:46 -0800

    comment some of the code and add an observeEvent to see which requests are being made

--- app.R
+++ app.R
@@ -5,12 +5,19 @@ library(forcats)  # reordering factors
 library(jsonlite) # easy json to dataframe
 library(brewr)    # homebrew json api wrapper
 library(readr)    # convert datatypes easily
-library(curl)
+library(curl)     # need so shiny wont complain
 
 # Define the UI
+# ------------------------------------------------------------------------------
 ui <- fluidPage(
+    
+    # Use custom CSS for Source Code Pro font 
     includeCSS("style.css"),
+    
+    # Create title 
     headerPanel("Homebrew Analytics"),
+    
+    # Contruct the left panel with all its options 
     sidebarPanel(
         p("This shiny app is a companion tool for the", 
         a(href = "https://github.com/tyluRp/brewr", "brewr"),
@@ -41,12 +48,21 @@ ui <- fluidPage(
                     value = c(1, 30))),
         div(DT::dataTableOutput("table"), style = "font-size:80%")
     ),
-    textOutput("endpoint"),
+    
+    # Store the plot on main panel 
     mainPanel(plotOutput("plot"))
 )
 
 # Define service logic
+# ------------------------------------------------------------------------------
 server <- function(input, output) {
+    
+    # Observe the endpoints for debugging purposes 
+    observeEvent(c(input$category, input$days), {
+        print(paste0("Endpoint: ", input$category, input$days))
+    })
+    
+    # Render the datatable 
     output$table <- DT::renderDataTable(DT::datatable({
         fromJSON(brewr(paste0(input$category, input$days)))[["items"]]
     }, extensions = c("Scroller"), 
@@ -54,6 +70,8 @@ server <- function(input, output) {
                       deferRender = TRUE, 
                       scrollY = 320, 
                       dom = "lftp")))
+    
+    # Render the plot 
     output$plot <- renderPlot({
         df <- fromJSON(brewr(paste0(input$category, input$days)))[["items"]]
         df <- readr::type_convert(df)
